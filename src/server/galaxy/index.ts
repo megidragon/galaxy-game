42d104a9a503807d7ddeaaadab0a80e3470a7920
megidragon <agustinh129@gmail.com>
2026-03-21 16:32:30 +0100

    Fresh start

--- src/server/galaxy/index.ts
+++
@@ -1,4 +0,0 @@
-export { generateGalaxy } from "./generator.js";
-export type { GalaxyData } from "./generator.js";
-export { saveGalaxy, loadGalaxy, isGalaxyGenerated } from "./persistence.js";
-export { generateSystemName, generatePlanetName } from "./names.js";
